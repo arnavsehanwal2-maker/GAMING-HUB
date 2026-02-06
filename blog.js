62f50a3fc0e5a6f25dbddbcbea642d83123451d1
arnavsehanwal2-maker <arnavsehanwal2@gmail.com>
2026-02-06 20:33:10 +0530

    Create blog.js

---
+++ blog.js
@@ -0,0 +1,13 @@
+export default function Blog() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Gaming Blog</h1>
+
+      <p>Top 10 Mobile Games in 2026</p>
+      <p>Best Free Fire Tips for Beginners</p>
+      <p>BGMI Pro Settings Guide</p>
+      <p>Top Offline Android Games</p>
+      <p>Best Gaming Apps for Low-End Phones</p>
+    </div>
+  );
+}
