b554abf42a7c556c9515c91dc3b49b127d9a946f
arnavsehanwal2-maker <arnavsehanwal2@gmail.com>
2026-02-06 10:08:31 +0530

    Add robots.ts for metadata route configuration

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://gaming-hub-flax.vercel.app/sitemap.xml',
+  }
+}
